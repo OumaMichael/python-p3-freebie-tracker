39013677fe11593d431a79696f9eb4d22bca90f3
Michael Ouma <Oumamichael108@gmail.com>
2025-05-24 19:10:52 +0545

    Updated seed file

--- lib/seed.py
+++ lib/seed.py
@@ -18,25 +18,25 @@ session.query(Company).delete()
 session.query(Dev).delete()
 
 # Create companies
-google = Company(name="Google", founding_year=1998)
-apple = Company(name="Apple", founding_year=1976)
-microsoft = Company(name="Microsoft", founding_year=1975)
+ODM = Company(name="ODM", founding_year=2005)
+UDA = Company(name="UDA", founding_year=2022)
+DCP = Company(name="DCP", founding_year=2025)
 
 # Create devs
-alice = Dev(name="Alice")
-bob = Dev(name="Bob")
-charlie = Dev(name="Charlie")
+Raila = Dev(name="Raila")
+Ruto = Dev(name="Ruto")
+Rigachi = Dev(name="Rigachi")
 
 # Add to session
-session.add_all([google, apple, microsoft, alice, bob, charlie])
+session.add_all([ODM, UDA, DCP, Raila, Ruto, Rigachi])
 session.commit()
 
 # Create freebies
-freebie1 = Freebie(item_name="T-shirt", value=25, dev=alice, company=google)
-freebie2 = Freebie(item_name="Stickers", value=5, dev=alice, company=apple)
-freebie3 = Freebie(item_name="Water Bottle", value=15, dev=bob, company=google)
-freebie4 = Freebie(item_name="Laptop Bag", value=50, dev=charlie, company=microsoft)
-freebie5 = Freebie(item_name="Pen", value=3, dev=bob, company=apple)
+freebie1 = Freebie(item_name="ODM T-shirts", value=2500000, dev=Raila, company=ODM)
+freebie2 = Freebie(item_name="CDF funds", value=5000000, dev=Raila, company=UDA)
+freebie3 = Freebie(item_name="Hustler funds", value=1500000, dev=Ruto, company=ODM)
+freebie4 = Freebie(item_name="DCP Tanks", value=500000, dev=Rigachi, company=DCP)
+freebie5 = Freebie(item_name="Wheelbarrow", value=300000, dev=Ruto, company=UDA)
 
 session.add_all([freebie1, freebie2, freebie3, freebie4, freebie5])
 session.commit()
