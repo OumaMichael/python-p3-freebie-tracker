ce4b05abe3fd848f9aa32f810856dfdb814135a5
Michael Ouma <Oumamichael108@gmail.com>
2025-05-24 18:33:57 +0545

    Updated debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -2,8 +2,8 @@
 
 from sqlalchemy import create_engine
 
-# from models import Company, Dev
+from models import Company, Dev
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///freebies.db')
+    engine = create_engine('sqlite:///lib/freebies.db')
     import ipdb; ipdb.set_trace()
